01f19c3b5c88d6af54d31a7238850e88e5bdf76f
SwaminathanNagarajan <swamiin2@gmail.com>
2025-08-19 21:41:50 +0530

    Updated CheckNumberIsPrime.java with latest changes

--- Week1/day2/CheckNumberIsPrime.java
+++ Week1/day2/CheckNumberIsPrime.java
@@ -4,6 +4,21 @@ public class CheckNumberIsPrime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (int i = 1; i < 20; i++) {
+			boolean isPrime = true;
+			for (int j = 1; j <= i / 2; j++) {
+				if (i % j == 0) {
+					isPrime = false;
+				}
+
+			}
+			if (isPrime) {
+				System.out.println(i + " is a Prime Number");
+			} else {
+				System.out.println(i + " is not a Prime Number");
+			}
+
+		}
 
 	}
 
